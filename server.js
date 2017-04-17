be59c0b16a9be51b89ea1ddaef87643dc1eb761d
ILYA ERSHOV <ershov.ilya@gmail.com>
2017-04-17 16:51:48 +0300

    Простейший express сервер

---
+++ server.js
@@ -0,0 +1,14 @@
+﻿var express = require('express')
+var http = require('http')
+var path = require('path')
+
+var app = express()
+app.set('port', 3000)
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Server started at port: '+app.get('port'))
+})
+
+app.use(function(req, res){
+    res.end("Hello")
+})
