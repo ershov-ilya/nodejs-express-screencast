2c0e3380f17cc009e950d1522bcbdcd328ad8163
ILYA ERSHOV <ershov.ilya@gmail.com>
2017-04-17 16:56:16 +0300

    Простейшая маршрутизация

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ http.createServer(app).listen(app.get('port'), function(){
     console.log('Server started at port: '+app.get('port'))
 })
 
-app.use(function(req, res){
-    res.end("Hello")
+app.use(function(req, res, next){
+    if(req.url == '/'){
+        res.end("Hello")
+    } else {
+        next()
+    }
+})
+
+app.use(function(req, res, next){
+    if(req.url == '/test'){
+        res.end("test")
+    } else {
+        next()
+    }
 })
